db4fa51767d77e4a8eab7affc6fa3d0e9b0ca063
Yangqingsheng <371260005@qq.com>
2018-06-16 15:27:25 +0800

    This a test

---
+++ Learn/Test/Test/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person("杨清生");
+            p.Ff();
+            Console.ReadKey();
+           
+        }
+    }
+    public class Person
+    {
+        public Person(string name)
+        {
+            this.Name = name;
+        }
+        private string _name;
+
+        public string Name {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+        public void Ff()
+        {
+            Console.WriteLine("{0}这是一个测试方法",this.Name);
+        }
+    }
+}
